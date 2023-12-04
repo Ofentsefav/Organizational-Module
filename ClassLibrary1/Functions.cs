37ba4f553bde36c7b1493a5924d61808663186fa
Ofentsefav <134281118+Ofentsefav@users.noreply.github.com>
2023-12-04 17:03:17 +0000

    Add files via upload

---
+++ ClassLibrary1/Functions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Functions
+    {
+        ModuleDetails det = new ModuleDetails();
+        int study = 0;
+        int remaininghours = 0;
+        public int Self_Study()
+        {
+            study = (ModuleDetails.Module_Credits * 10) / (ModuleDetails.Total_semester_weeks - ModuleDetails.Module_Weekly_classhrs);
+            return study;
+        }
+        public int Remaning()
+        {
+            remaininghours = Self_Study() - ModuleDetails.Recorded_Hours;
+            return remaininghours;
+        }
+    }
+}
---
+++ ClassLibrary1/ModuleDetails.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class ModuleDetails
+    {
+        public static DateTime Semester_start_Date { get; set; }
+        public static string Module_Code { get; set; }
+        public static string Module_name { get; set; }
+        public static int Module_Credits { get; set; }
+        public static int Module_Weekly_classhrs { get; set; }
+        public static int Total_semester_weeks { get; set; }
+        public static DateTime Certain_Day { get; set; }
+        public static int Recorded_Hours { get; set; }
+        
+        public ModuleDetails(DateTime semesstart, string code, string name, int credits, int weeklyclasshrs, int totsemesterweeks, DateTime dayselected, int recordedhrs)
+        {
+            Semester_start_Date = semesstart;
+            Module_Code = code;
+            Module_name = name;
+            Module_Credits = credits;
+            Module_Weekly_classhrs = weeklyclasshrs;
+            Total_semester_weeks = totsemesterweeks;
+            Certain_Day = dayselected;
+            Recorded_Hours = recordedhrs;
+
+
+        }
+
+        public ModuleDetails()
+        {
+        }
+    }
+}
